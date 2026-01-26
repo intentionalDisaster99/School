dc8791ea4b73342cb2c76f7a93272d7cf9b532f8
Sam Whitlock <abyssalflerken@gmail.com>
2026-01-26 11:16:40 -0600

    Completed Programming3 - Still needs refactoring

---
+++ CS 309/Homework/Programming3/src/Main.java
@@ -0,0 +1,144 @@
+/**
+ * <h1>Program Three</h1>
+ * This is a simple program to read from a custom file inputted by the user to then store and print
+ * student data. It is built off of my Student class from my Program 2 submission
+ *
+ * @author  Sam Whitlock
+ * @version 1.0.0
+ * @since   2026-01-25
+ */
+
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Creating a scanner to read input
+        Scanner pancake = new Scanner(System.in);
+
+        // Reading the input
+        System.out.println("Please input the path to the input file.\n");
+        String path = pancake.nextLine();
+
+        // Some simple error checking
+        while (!path.endsWith(".txt")) {
+            System.out.println("Make sure that the path is to a .txt file, please.");
+            System.out.println("Please input the path to the input file.\n");
+            path = pancake.nextLine();
+        }
+
+        // The ArrayList that we are using to store the student data
+        ArrayList<Student> students = new ArrayList<>();
+
+        // Opening the file if it exists
+        File inputFile = new File(path);
+
+        // Telling them that they have an error in the file path until we can open the file
+        boolean valid = false;
+        while (!valid) {
+
+            // Trying to open the file
+            try (Scanner fileReader = new Scanner(inputFile)) {
+
+                System.out.println("Looping");
+                // We got to a file, so we are good to break out after this iteration
+                valid = true;
+
+                // Because we know that there is one student on each line, we can
+                // go line by line to get each student
+                while (fileReader.hasNextLine()) {
+
+                    // Now we need to parse the student
+
+                    // Everything is split by spaces, so we can just use split
+                    String[] tokens = fileReader.nextLine().split(" ");
+
+                    System.out.println("Working on " + tokens[0]);
+
+                    // Some simple error checking
+                    if (tokens.length < 2) {
+                        System.out.println("Bad input detected, ensure each line has a first and last name!");
+                        return;
+                    }
+
+                    // We will create a default student that we can add data to
+                    Student newStudent = new Student();
+
+                    // The first two tokens are just the first and last name
+                    newStudent.setFirstName(tokens[0]);
+                    newStudent.setLastName(tokens[1]);
+
+                    // The rest of the tokens are just test scores, so we can 
+                    // simply insert the tokens parsed as doubles
+                    for (int i = 2; i < tokens.length; i++) {
+                        newStudent.addTest(Double.parseDouble(tokens[i]));
+                    }
+
+                    // Adding the student to the list of students
+                    students.add(newStudent);
+
+                }
+
+            } catch (FileNotFoundException e) {
+
+                // Prompting the user to ask if they want to try again
+                System.out.println(
+                        "Unfortunately, the file was not found at that path.\nWould you like to input a different path?(y/n)\n");
+                String ans = pancake.nextLine();
+                while (!ans.equalsIgnoreCase("y") && !ans.equalsIgnoreCase("n")) {
+                    System.out.println("Please only respond with a \"y\" or a \"n\"");
+                    System.out.println("Got '" + ans +"'");
+                    ans = pancake.nextLine();
+                }
+
+                // Exiting if they don't want to
+                if (ans.equalsIgnoreCase("n")) {
+                    return;
+                }
+
+                // Getting a new path
+                System.out.println("Please input the path to the input file.\n");
+                path = pancake.nextLine();
+
+                // Some simple error checking
+                while (!path.endsWith(".txt")) {
+                    System.out.println("Make sure that the path is to a .txt file, please.");
+                    System.out.println("Please input the path to the input file.\n");
+                    path = pancake.nextLine();
+                }
+
+                // Now it will go back to the beginning of the loop to try to open it again
+
+            }
+        }
+
+        // Closing the scanner
+        pancake.close();
+
+        // Displaying the students' information
+        for (Student s : students) {
+            displayStudent(s);
+        }
+
+    }
+
+    // Helper function to display a single student's information
+    public static void displayStudent(Student student) {
+        System.out.print(student.getFullName());
+        System.out.print(" has " + student.getTestCount() + " grades");
+        System.out.println(" with an average of " + student.getAverage());
+    }
+
+}
+
+
+
+
+
+
+
+
+
---
+++ CS 309/Homework/Programming3/src/Student.java
@@ -0,0 +1,130 @@
+
+/**
+* <h1>Program Two</h1>
+* This is an updated simple Student class made for my CS 321 class as my submission for the Program 2 Assignment. It is built off of my Program One submission.
+*
+* @author  Sam Whitlock
+* @version 1.0.0
+* @since   2026-01-14
+*/
+
+import java.util.ArrayList;
+
+public class Student {
+    
+
+
+    /**
+     * Creates a new Student object based on the inputted parameters.
+     * @param firstName The first name of the student
+     * @param lastName The last name of the student
+     */
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grades = new ArrayList<Double>();
+    }
+
+    
+    /**
+     * Creates a new default Student object. 
+     * Default values:
+     *      firstName -- String "unknown"
+     *      lastName  -- String "unknown"
+     *      grades    -- Empty ArrayList<Double>
+     */
+    public Student() {
+        firstName = "unknown";
+        lastName = "unknown";
+        grades = new ArrayList<Double>();
+    }
+   
+    /**
+     * Sets the first name of the student object
+     * @param newFirstName The new first name of the student
+     */
+    public void setFirstName(String newFirstName) {
+        this.firstName = newFirstName;
+    }
+
+    /**
+     * Sets the last name of the student object
+     * @param newLastName The new last name of the student
+     */
+    public void setLastName(String newLastName) {
+        lastName = newLastName;
+    }
+
+    /**
+     * Sets the grades of the student
+     * @param newGrade The new grade to add to the Student's record
+     */
+    public void addTest(double newGrade) {
+        grades.add(newGrade);
+    }
+
+    /**
+     * Sets the grades of the student
+     * @param newGrade The new grade to add to the Student's record
+     */
+    public void addTest(int newGrade) {
+        grades.add((double)newGrade);
+    }
+
+
+    /**
+     * Returns a formatted name of the student in the format 
+     * "lastName, firstName"
+     * @returns String The formatted name of the student
+     */
+    public String getFullName() {
+        return lastName + ", " + firstName;
+    }
+
+    /**
+     * Gets the average grade of the student
+     * @returns double The average grade of the student between the three grades
+     */
+    public double getAverage() {
+        // Checking the length of grades to ensure no divide by zero error
+        // Will never be an issue for this assignment, but for future usage it allows expansion
+        if (grades.size() == 0) {
+            return 0;
+        }
+
+        // The output of the method, will eventually be the average
+        double out = 0;
+        for (int i = 0; i < grades.size(); i++) {
+            out += grades.get(i);
+        }
+        out /= (double)grades.size();
+        return out;
+    }
+
+    /**
+     * Gets the number of grades of the Student
+     * @returns int The number of test grades the Student has 
+     */
+    public int getTestCount() {
+        return grades.size();
+    }
+    
+
+    /**
+     * Returns a String defining the Student object
+     * Not technically necessary for the assignment, but Professor Allen said it was wise to add them in our classes
+     * @returns String String defining the Student object
+     */
+    public String toString() {
+        return "Student: "
+                + "firstName: \"" + firstName
+                + "\" lastName: \"" + lastName
+                + "\" Grades:\n" + grades.toString();
+    }
+
+    // Private instance variables
+    private String lastName;
+    private String firstName;
+    private ArrayList<Double> grades; 
+
+}
