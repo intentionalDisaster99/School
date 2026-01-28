00c7a09dd555cc19a08efe4d69394cbf6d816cea
Sa9m <abyssalflerken@gmail.com>
2026-01-28 12:21:20 -0600

    Refactored Main and Student to be ready for submission, no longer back compatible with Student 1.0.0

--- CS 321/Homework/Programming3/src/Student.java
+++ CS 321/Homework/Programming3/src/Student.java
@@ -4,7 +4,7 @@
 * This is an updated simple Student class made for my CS 321 class as my submission for the Program 2 Assignment. It is built off of my Program One submission.
 *
 * @author  Sam Whitlock
-* @version 1.0.0
+* @version 1.1.0
 * @since   2026-01-14
 */
 
@@ -116,10 +116,17 @@ public class Student {
      * @returns String String defining the Student object
      */
     public String toString() {
-        return "Student: "
-                + "firstName: \"" + firstName
-                + "\" lastName: \"" + lastName
-                + "\" Grades:\n" + grades.toString();
+        return "Student: " + "firstName: \"" + firstName + "\" lastName: \"" + lastName + "\" Grades:\n"
+                + grades.toString();
+    }
+    
+    /**
+     * Displays the student information in the format matching Programming 3
+     */
+    public void display() {
+        System.out.print(this.getFullName());
+        System.out.print(" has " + this.getTestCount() + " grades");
+        System.out.println(" with an average of " + this.getAverage());
     }
 
     // Private instance variables
